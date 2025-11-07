b35da4866863dae2606c1cce410f1772766be2b5
Julien Kmec <me@julien.dev>
2025-11-07 12:47:09 -0800

    fix

--- partial_tables/partial_table.py
+++ partial_tables/partial_table.py
@@ -15,41 +15,60 @@ class PartialTable:
 
 
 class PartialBase:
-    def __init_subclass__(cls, **kwargs):
-        # For partial tables, rewrite annotations to Optional and inject mapped_column(nullable=True)
-        if issubclass(cls, PartialTable):
-            type_hints = get_type_hints(cls, include_extras=True)
-            raw_annotations = dict(getattr(cls, "__annotations__", {}))
+    """
+    Base class for all partial tables.
 
-            def rewrite_with_optional(a: object) -> object:
-                origin = get_origin(a)
-                if origin is Annotated:
-                    base, *meta = get_args(a)
-                    if any(isinstance(m, PartialAllowed) for m in meta):
-                        return Annotated[
-                            Optional[base], mapped_column(nullable=True), *meta
-                        ]
-                    new_base = rewrite_with_optional(base)
-                    if new_base is not base:
-                        return Annotated[new_base, *meta]
-                    return a
-                args = get_args(a)
-                if not args:
-                    return a
-                new_args = tuple(rewrite_with_optional(x) for x in args)
-                if new_args != args and origin is not None:
-                    try:
-                        return origin[tuple(new_args)]  # type: ignore[index]
-                    except Exception:
-                        return a
+    Any class that sub-classes this base class will have all fields that have the PartialAllowed() annotation set to nullable.
+    """
+
+    def __init_subclass__(cls, **kwargs):
+        if not issubclass(cls, PartialTable):
+            super().__init_subclass__(**kwargs)
+            return
+
+        type_hints = get_type_hints(cls, include_extras=True)
+        raw_annotations = dict(getattr(cls, "__annotations__", {}))
+
+        def _rewrite_with_optional(a: object) -> object:
+            origin = get_origin(a)
+
+            if origin is Annotated:
+                base, *meta = get_args(a)
+
+                if any(isinstance(m, PartialAllowed) for m in meta):
+                    return Annotated[
+                        Optional[base], mapped_column(nullable=True), *meta
+                    ]
+
+                new_base = _rewrite_with_optional(base)
+
+                if new_base is not base:
+                    return Annotated[new_base, *meta]
+
                 return a
 
-            for name, ann in type_hints.items():
-                new_ann = rewrite_with_optional(ann)
-                if new_ann is not ann:
-                    raw_annotations[name] = new_ann
-                    # Override the inherited mapped_column assignment on this subclass
-                    setattr(cls, name, mapped_column(nullable=True))
-            if raw_annotations:
-                cls.__annotations__ = raw_annotations
+            args = get_args(a)
+
+            if not args:
+                return a
+
+            new_args = tuple(_rewrite_with_optional(x) for x in args)
+
+            if new_args != args and origin is not None:
+                if hasattr(origin, "__class_getitem__"):
+                    return origin[tuple(new_args)]  # type: ignore[index]
+
+            return a
+
+        for name, ann in type_hints.items():
+            new_ann = _rewrite_with_optional(ann)
+
+            if new_ann is not ann:
+                raw_annotations[name] = new_ann
+                # Override the inherited mapped_column assignment on this subclass
+                setattr(cls, name, mapped_column(nullable=True))
+
+        if raw_annotations:
+            cls.__annotations__ = raw_annotations
+
         super().__init_subclass__(**kwargs)
